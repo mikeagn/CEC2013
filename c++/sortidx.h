e00c680fabe8b90bef3d00eb7251a1af27a10f9d
Mike @ darth <m.epitropakis@lancaster.ac.uk>
2016-10-27 22:43:35 +0100

    Fixed headers in C++ files

--- c++/sortidx.h
+++ c++/sortidx.h
@@ -1,8 +1,8 @@
 /******************************************************************************
- * Version: 1.0
- * Last modified on: 21 January, 2013 
+ * Version: 1.0.1
+ * Last modified on: 27	October, 2016 
  * Developers: Michael G. Epitropakis, Xiaodong Li.
- *      email: mge_(AT)_cs_(DOT)_stir_(DOT)_ac_(DOT)_uk 
+ *      email: m_(DOT)_epitropakis_(AT)_lancaster_(DOT)_ac_(DOT)_uk 
  *           : xiaodong_(DOT)_li_(AT)_rmit_(DOT)_edu_(DOT)_au 
  * ***************************************************************************/
 #ifndef __SORT_IND_H__
